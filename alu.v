2fc6da86e4a60491a894c8cca8d8a700bc4f170c
Allen Zhong <allen.zhong1@gmail.com>
2023-02-24 19:23:57 -0500

    Update alu.v
    Corrected functionality of ALU using two MUXes to select operation.

--- alu.v
+++ alu.v
@@ -1,42 +1,32 @@
-module alu(a,b,alu_sel, c_hi, c_lo);
+module alu(a, b, alu_sel, c_hi, c_lo);
   input [31:0] a;
   input [31:0] b;
-  input [1:0] alu_sel;
-  reg [31:0] output_hi;
-  reg [31:0] output_lo;
-  output wire [31:0] c_hi;
-  output wire [31:0] c_lo;
-  reg [63:0] temp;
-  assign c_hi = output_hi;
-  assign c_lo = output_lo;
-  always@(alu_sel or a or b) begin
-    case(alu_sel)
-	   2'b00 : begin
-		   temp = a+b;
-			output_hi= temp[63:32];
-			output_lo = temp[31:0];
-		end
-		2'b01 : begin
-			temp = a-b;
-			output_hi= temp[63:32];
-			output_lo = temp[31:0];
-		end
-		2'b10 : begin
-			temp = a*b;
-			output_hi= temp[63:32];
-			output_lo = temp[31:0];
-		end
-		2'b11 : begin
-			temp = a/b;
-			output_hi= temp[63:32];
-			output_lo = temp[31:0];
-		end
-		default : begin
-			output_hi= 32'h00000000;
-			output_lo = 32'h00000000;
-		end
-	endcase
-  end
+  input [3:0] alu_sel;
+  output [31:0] c_hi;
+  output [31:0] c_lo;
+  wire c_in;
+  wire c_out;
+  wire [31:0] add_out;
+  wire [31:0] sub_out;
+  wire [63:0] mul_out;
+  wire [31:0] div_out;
+  wire [30:0] tmp0;
+  wire [31:0] tmp1;
+  wire [31:0] tmp2;
+  wire enable;
+  wire bo_in;
+  wire bo_out;
+  assign c_in = 0;
+  assign tmp0 = 0;
+  assign tmp1 = 0;
+  assign tmp2 = 0;
+  assign bo_in = 1;
+  rca_32_bit add(.c_out(c_out), .sum(add_out), .a(a), .b(b), .c_in(c_in));
+  subtractor sub(.bo_out(bo_out), .d(sub_out), .a(a), .b(b), .bo_in(bo_in));
+  multiplier mul(.a(a), .b(b), .m(mul_out));
+  divider    div(.a(a), .b(b), .q(div_out));
+  mux4_to_1  mux0(.mux_out(c_lo), .data3(add_out), .data2(sub_out), .data1(mul_out[31:0]), .data0(div_out), .select(alu_sel));
+  mux4_to_1  mux1(.mux_out(c_hi), .data3({tmp0,c_out}), .data2({tmp1,bo_out}), .data1(mul_out[63:32]), .data0(tmp2), .select(alu_sel));
 endmodule 
 	
   
