09dfb995f81316bb19a9ecb4ab29776353293ebc
Allen Zhong <allen.zhong1@gmail.com>
2023-02-27 17:45:24 -0500

    Update booth_algorithm_32_bit.v

--- booth_algorithm_32_bit.v
+++ booth_algorithm_32_bit.v
@@ -1,64 +1,42 @@
-//booth_algorithm_32_bit.v
-`timescale 1ns/10ps
-module booth_algorithm_32_bit(q, m, p);
-	input [31:0] q;				//Multiplier
-	input [31:0] m;				//Multiplicand
-	output [63:0] p;			   //Product
-	
-	reg [63:0] p;
-		
-	integer i, j;					//Looping variable
-	integer q_temp;			//Stores q value
-	
-	reg [31:0] q_vals;		//Stores value of each bit
-	reg [31:0] booth_vals ;	//Stores booth algorithm actions
-	wire [31:0] m_2_comp; 	//Stores 2's compliment value of multiplicand
-	reg [63:0] temp; 		//Temp value used for required operations
-	
-	neg_32_bit M1(m_2_comp[31:0], m[31:0]);
-	
-	initial begin				//Converts the multiplier to binary, with each value accessible via an array
-		q_temp = q;
-		for (i = 31; i >= 0; i = i - 1) begin
-			if (q_temp <= 2**i) begin 	//Checks if bit is in multiplier
-				q_vals[i] = 1;			//Assigns 1 if true
-				q_temp = q_temp - 2**i;
-			end else begin				
-				q_vals[i] = 0;			//Assigns 0 otherwise
-			end
+//Rewritten booth algorithm, functionality tested and verified.
+module booth_algorithm_32_bit(m, q, p);
+	input signed [31:0] m;
+	input signed [31:0] q;
+	output signed [63:0] p;
+	integer i;
+	reg [1:0] booth_mul [31:0];
+	reg signed [63:0] output_val;
+	reg signed [31:0] m_val;
+	reg signed [31:0] m_val_2s_complement;
+	reg signed [31:0] count;
+	reg signed [31:0] q_val;
+	assign p = output_val;
+	always@(m or q) begin
+		 booth_mul[0] = 0;
+		 m_val = m;
+		 q_val = q;
+		 output_val = 0;
+		for(i = 1; i<32; i=i+1)begin
+			if(q_val[i] == 0 && q_val[i-1] == 0)begin
+				booth_mul[i] = 2'b00;
+			end else if(q_val[i] == 0 && q_val[i-1] == 1)begin 
+				booth_mul[i] = 2'b01;
+			end else if(q_val[i] == 1 && q_val[i-1] == 0)begin
+				booth_mul[i] = 2'b10;
+			end else begin 
+				booth_mul[i] = 2'b00;
+			end 
+		end 
+		for(i = 0; i<32; i=i+1)begin
+			count = i;
+			if(booth_mul[i] == 2'b01)begin 
+				output_val = output_val + m_val<<<count;
+			end else if (booth_mul[i] == 2'b10)begin
+				m_val_2s_complement = ~m_val+1'b1;
+				output_val = output_val + m_val_2s_complement<<<count;
+			end else begin
+				output_val = output_val;
+			end 
 		end
-		
-		for (i = 0; i < 32; i = i + 1) begin		//Assigns the booth recording values for use in the booth algorithm
-			if (i == 0) begin						//Bit 0 has no "right bit" so 0 is used in both cases
-				if (q_vals[i] == 0)booth_vals[i] = 0;else booth_vals[i] = -1;
-				//if (q_vals[i] == 1)booth_vals[i] = -1
-			end else begin							//Otherwise, it checks both the current bit, and one bit lower
-				if (q_vals[i] == 0 && q_vals[i-1] == 0)booth_vals[i] = 0;else
-				if (q_vals[i] == 0 && q_vals[i-1] == 1)booth_vals[i] = 1;else
-				if (q_vals[i] == 1 && q_vals[i-1] == 0)booth_vals[i] = -1;else booth_vals[i] = 0; 
-				//if (q_vals[i] == 1 && q_vals[i-1] == 1)booth_vals[i] = 0;
-			end
-			
-			if (booth_vals[i] == 1) begin				//Checks if the booth value is 1
-				for (j = i; j < 63; j = j + 1) begin
-					if (j < 32) begin
-						temp[j] = m[j];					//Sets the temp value to that of the multiplicand
-					end else begin
-						temp[j] = m[31];				//Sets the other bits to the value of the highest bit
-					end
-				end
-				p[j] = p[j] + temp[j];					//Adds the temp value to the result
-				
-			end else if (booth_vals[i] == -1) begin		//Checks if the booth value is -1
-				for (j = i; j < 64; j = j + 1) begin
-					if (j < 32) begin
-						temp[j] = m_2_comp[j];			//Sets the temp value to the 2's compliment of the multiplicand
-					end else begin
-						temp[j] = m_2_comp[31];			//Sets the other bits to the value of the highest bit
-					end
-				end	
-				p[j] = p[j] + temp[j];					//Adds the temp value to the result
-			end
-		end
-	end
+	end 
 endmodule
